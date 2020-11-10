1bfcaca3760a69c60dd173aa8b2d6184e093c3a3
LuHeFr <lukas.h@online.de>
2020-11-10 21:09:43 +0100

    Add Monte-Carlo-Pi computation

--- main.cpp
+++ main.cpp
@@ -12,7 +12,17 @@ int main() {
     return dist(rng);
   };  // Lamdaexpression - hier definierte Funktion
 
-  for (int i = 0; i < 10; ++i) {
-    cout << random() << '\n';
+  int n = 100000000;  // Anzahl der Zahlen
+  int c = 0;          // Zählvariable für innerhlab des Kreises
+
+  for (int i = 0; i < n; ++i) {
+    // cout << random() << '\n';
+    const auto x = random();
+    const auto y = random();
+    const auto r2 = x * x + y * y;
+    if (r2 < 1) ++c;
   }
+
+  auto monte_carlo_pi = 4 * float(c) / float(n);
+  cout << "pi = " << monte_carlo_pi << '\n';
 }
